02815fab0214211443a594ef7b41ed4f04a01a74
zhengbi-yong <zhengbi.yong@outlook.com>
2024-04-14 12:05:03 +0800

    Remove Feedback Dialog.

--- slog-carbon/src/gatsby-theme-carbon/components/FeedbackDialog/FeedbackDialog.js
+++ slog-carbon/src/gatsby-theme-carbon/components/FeedbackDialog/FeedbackDialog.js
@@ -2,11 +2,7 @@ import React from 'react';
 import ThemeFeedbackDialog from 'gatsby-theme-carbon/src/components/FeedbackDialog/FeedbackDialog';
 
 const FeedbackDialog = ({ props }) => {
-  const onSubmit = (data) => {
-    console.log({ ...data });
-  };
-
-  return <ThemeFeedbackDialog {...props} onSubmit={onSubmit} />;
+  return <ThemeFeedbackDialog {...props}/>;
 };
 
 export default FeedbackDialog;
