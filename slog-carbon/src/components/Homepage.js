6b9c6df1c46dd2c314f89860a1f344b8657ccc89
zhengbi-yong <64255868+zhengbi-yong@users.noreply.github.com>
2024-04-15 14:21:57 +0800

    Migrate this site to v4.

--- slog-carbon/src/components/Homepage.js
+++ slog-carbon/src/components/Homepage.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import { HomepageBanner, HomepageCallout } from 'gatsby-theme-carbon';
 import HomepageTemplate from 'gatsby-theme-carbon/src/templates/Homepage';
-import { calloutLink } from './Homepage.module.scss';
-
+import { AnchorLink, AnchorLinks } from 'gatsby-theme-carbon';
 import Carbon from './carbon.jpg';
 
 const FirstLeftText = () => <p>个人简介</p>;
@@ -10,11 +9,9 @@ const FirstLeftText = () => <p>个人简介</p>;
 const FirstRightText = () => (
   <p>
     <code>清华大学</code>自动化系2019级本科生，<code>北京理工大学</code>自动化学院2024级硕士生。研究兴趣：深度学习、强化学习、机器人、计算机视觉和多模态。
-    <a
-      className={calloutLink}
-      href="https://zhengbi-yong.github.io">
-      了解更多 →
-    </a>
+    <AnchorLinks>
+      <AnchorLink to='https://zhengbi-yong.github.io'>了解更多</AnchorLink>
+    </AnchorLinks>
   </p>
 );
 
